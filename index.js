f6f9bd68b789c68de2d1b5cb700205fcbb6741f3
Joe Megyery <104165842+Joemegz@users.noreply.github.com>
2023-03-01 21:52:34 -0600

    code update

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const Models = require("./models.js");
  // { useNewUrlParser: true, useUnifiedTopology: true }
 //);
 
-mongoose.connect('process.env.CONNECTION_URI', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const Movies = Models.Movie;
 const Users = Models.User;
