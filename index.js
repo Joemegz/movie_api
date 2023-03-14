c77265d632e0e7609e5128a0fdb3aa35c1b59934
Joe Megyery <104165842+Joemegz@users.noreply.github.com>
2023-03-13 22:36:47 -0500

    get movie name by genre

--- index.js
+++ index.js
@@ -194,21 +194,21 @@ app.get(
 );
 
 // Get a Movie by Genre
-// app.get(
-//   "/movies/genre/:Name",
-//   passport.authenticate("jwt", { session: false }),
-//   (req, res) => {
-//     Movies.findOne({ Name: req.params.Name })
-//       .then((movie) => {
-//         res.json(movie.Genre);
-//         console.log("inside then")
-//       })
-//       .catch((err) => {
-//         console.error(err);
-//         res.status(500).send("Error: " + err);
-//       });
-//   }
-// );
+app.get(
+  "/movies/genre/:Name",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Movies.findOne({ Name: req.params.Name })
+      .then((movie) => {
+        res.json(movie.Title);
+        console.log("inside then")
+      })
+      .catch((err) => {
+        console.error(err);
+        res.status(500).send("Error: " + err);
+      });
+  }
+);
 
 
 //Get genre description
